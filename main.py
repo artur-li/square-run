a1478eaed29c7946af9e22282b418fb7c8a9cc30
Artur <arturli.official@gmail.com>
2023-04-11 11:32:56 -0400

    created obstacles

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 pygame.init()
 
 # screen
@@ -30,19 +30,72 @@ player_group = pygame.sprite.Group()
 player = Player()
 player_group.add(player)
 
+class Obstacle_a(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((80,80))
+        self.image.fill("red")
+        self.rect = self.image.get_rect(bottomleft=(800,300))
+    def movement(self):
+        self.rect.centerx -= 5
+    def kill(self):
+        if self.rect.centerx <= 0:
+            pygame.sprite.Sprite.kill(self)
+    def update(self):
+        self.movement()
+        self.kill()
+class Obstacle_b(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((80,80))
+        self.image.fill("red")
+        self.rect = self.image.get_rect(bottomleft=(800,220))
+    def movement(self):
+        self.rect.centerx -= 5
+    def kill(self):
+        if self.rect.centerx <= 0:
+            pygame.sprite.Sprite.kill(self)
+    def update(self):
+        self.movement()
+        self.kill()
+timer = 0
+interval = 1
+def spawn_obstacle():
+    global timer
+    global interval
+    timer += interval
+    if timer % 1440 == 0 and interval <= 2.5:
+        interval += 0.5
+    if timer % 180 == 0:
+        if random.choice([1,2]) == 1:
+            obstacle = Obstacle_a()
+            obstacle_group.add(obstacle)
+        else:
+            obstacle = Obstacle_b()
+            obstacle_group.add(obstacle)
+obstacle_group = pygame.sprite.Group()
+
 # game loop
 while True:
 
+    # close window when 'x' presed
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    # draw to screen
+    # background
     screen.fill("black")
     pygame.draw.line(screen,"white",(0,300),(800,300),1)
+    # player
     player_group.draw(screen)
     player_group.update()
+    # obstacles
+    spawn_obstacle()
+    obstacle_group.draw(screen)
+    obstacle_group.update()
         
-    # 60fps
+    # update screen 60fps
     pygame.display.update()
     clock.tick(60)
